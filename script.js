5abc914e84e69fc1b46902fd969e0496191de137
Cyril Dela Cruz <188366372+Cyr-hub@users.noreply.github.com>
2025-04-03 01:22:57 +0800

    hey

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 gsap.from(".greeting", { opacity: 0, y: 50, duration: 1, ease: "power3.out" });
-gsap.from(".role", { opacity: 0, y: 50, duration: 1, ease: "power3.out", delay: 0.3 });
+gsap.from(".role", { opacity: 0, y: 50, duration: 1, ease: "power3.out", delay: 0.6 });
 gsap.from(".description", { opacity: 0, y: 50, duration: 1, ease: "power3.out", delay: 0.6 });
-gsap.from(".profile-image", { opacity: 0, y: 50, duration: 0.9, ease: "power3.out", delay: 0.6 });
+gsap.from(".profile-image", { opacity: 0, y: 50, duration: 0.1, ease: "power3.out", delay: 0.6 });
     
 gsap.from(".social-icons a", { 
     opacity: 0, 
     y: 20, 
-    stagger: 0.2, /* Adds a delay between each icon */
+    stagger: 0.2,
     duration: 0.8, 
     ease: "power2.out",
     delay: 0.9
@@ -32,3 +32,6 @@ gsap.from(".buttons a", {
     ease: "back.out(1.7)", 
     delay: 1.2 
     });
+
+
+    
